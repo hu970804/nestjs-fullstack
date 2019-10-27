488518a65f12a60d8b8c5f0f95f78fa00e20429a
hu970804 <812510003@qq.com>
2019-10-27 22:04:04 +0800

    [feat]: init project and add user CRUD

---
+++ server/apps/admin/src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('视频后台管理 API')
+    .setDescription('供后台管理界面调用的服务端 API')
+    .setVersion('1.0')
+    .addTag('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+  await app.listen(3000);
+}
+bootstrap();
