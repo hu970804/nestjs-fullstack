ba83ae6fd1aa8403c54027bdd66b90bf82bcca46
hu970804 <812510003@qq.com>
2019-10-27 22:54:21 +0800

    [feat]: add 课程和课时 CRUD

--- server/apps/admin/src/app.module.ts
+++ server/apps/admin/src/app.module.ts
@@ -3,13 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DbModule } from '@libs/db';
 import { UsersModule } from './users/users.module';
+import { CoursesModule } from './courses/courses.module';
+import { EpisodesController } from './episodes/episodes.controller';
+import { EpisodesModule } from './episodes/episodes.module';
 
 @Module({
   imports: [
     DbModule,
     UsersModule,
+    CoursesModule,
+    EpisodesModule,
   ],
-  controllers: [AppController],
+  controllers: [AppController, EpisodesController],
   providers: [AppService],
 })
 export class AppModule {}
